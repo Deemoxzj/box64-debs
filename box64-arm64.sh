7b91709911f7d9ba6912f8ada3b820505726e4d9
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2025-06-16 18:55:17 +0800

    Update box64-arm64.sh

--- box64-arm64.sh
+++ box64-arm64.sh
@@ -11,5 +11,5 @@ mv v0.3.6.tar.gz box64
 cd box64
 mkdir build
 cd build
-cmake .. -DARM64=ON -DBAD_SIGNAL=ON -DBOX32=ON -DBOX32_BINFMT=ON -DSD8G2=ON -DARM_DYNAREC=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWOW --install-prefix=/usr/
+cmake .. -DWOW64=ON -DARM64=ON -DBAD_SIGNAL=ON -DBOX32=ON -DBOX32_BINFMT=ON -DSD8G2=ON -DARM_DYNAREC=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWOW --install-prefix=/usr/
 sudo checkinstall make install
